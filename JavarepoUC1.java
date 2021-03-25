576de1f61ef6b749b58e2aae4acb0eba374b827e
vaishnavi birle <vaishnavibirle1133@gmail.com>
2021-03-25 11:02:27 +0530

    [vaishnavi] added JavarepoUC1.java

---
+++ JavarepoUC1.java
@@ -0,0 +1,16 @@
+public class JavarepoUC1
+{
+	public static void main(String[] args)
+	{
+		int empNumber = 0;
+		System.out.println("Enter number");
+		if(empNumber>5)
+		{
+			System.out.println("WELCOME");
+		}
+		else
+		{
+			System.out.println("not allowed");
+		}
+	}
+}
